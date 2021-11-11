fe43a8a9bdd4ecec1499c022e6529ac69d20dd10
tkansaki <tkansaki@hawaii.edu>
2021-11-11 05:27:57 -1000

    Cards displayed

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,16 +1,16 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Clubs } from '../../api/club/Clubs.js';
 
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
 function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
+  console.log(`  Adding: ${data.name}`);
+  Clubs.collection.insert(data);
 }
 
 // Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
+if (Clubs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
