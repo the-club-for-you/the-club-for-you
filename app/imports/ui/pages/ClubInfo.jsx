51663f6ad72f46264d85761a279541e9e644e1cd
malialiu <mliu0141@gmail.com>
2021-11-11 22:46:18 -1000

    mockup club's information page

--- app/imports/ui/pages/ClubInfo.jsx
+++ app/imports/ui/pages/ClubInfo.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Header, Button, Label } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class ClubInfo extends React.Component {
   render() {
     return (
-      <Grid container className='clubsInfo'>
-        <Grid.Row>
-          <Grid.Column>
-            <Header as={'h1'}>Clubs Name</Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <Header as={'h2'}>Purpose</Header>
-            <Header as={'h2'}>Contact Information</Header>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <div className="clubsInfo-background">
+        <Grid container id='landing-page' verticalAlign="middle" className="clubsInfo">
+          <div className='informations'>
+            <Grid.Row>
+              <br/>
+              <Header as='h1'>Name of Organization</Header>
+            </Grid.Row>
+            <br/>
+            <Grid.Row>
+              <Label color='green' size='big'>Type</Label>
+            </Grid.Row>
+            <br/>
+            <Grid.Row>
+              <Header as='h3'>Club purpose:{}</Header>
+            </Grid.Row>
+            <br/>
+            <Grid.Row>
+              <Header as='h3'>Contact information:{}</Header>
+              <ul>
+                <li>Contact Person:{}</li>
+                <li>Contact Person`s Email:{}</li>
+              </ul>
+            </Grid.Row>
+            <br/> <br/> <br/>
+          </div>
+        </Grid>
+      </div>
     );
   }
 }
