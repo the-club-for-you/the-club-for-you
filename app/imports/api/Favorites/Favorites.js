0045bf9c2b61daa8dfbba0dad18533bf5e6c0238
jingzhef <jingzhef@hawaii.edu>
2021-11-30 11:01:26 -1000

    added favorites collection

---
+++ app/imports/api/Favorites/Favorites.js
@@ -0,0 +1,37 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+import { Types } from '../types/Types';
+
+const types = Types.list;
+
+class FavoritesCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'FavoritesCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+      approve: Date,
+      expire: Date,
+      type: { type: Array, optional: false },
+      'type.$': { type: String, allowedValues: types },
+      contact: String,
+      email: String,
+      description: String,
+      photo: String,
+      website: { type: String, optional: true },
+      owner: { type: String, optional: true },
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.FavoritesCollection = `${this.name}.publication.favorites`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Favorites = new FavoritesCollection();
--- app/imports/ui/components/ClubCard.jsx
+++ app/imports/ui/components/ClubCard.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { Card, Image, Label } from 'semantic-ui-react';
+import swal from 'sweetalert';
+import { Button, Card, Icon, Image, Label } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, NavLink } from 'react-router-dom';
+import { Favorites } from '../../api/Favorites/Favorites';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class ClubCard extends React.Component {
+
+  submit(data) {
+    const name = this.props.club.name;
+    const approve = this.props.club.approve;
+    const expire = this.props.club.expire;
+    const type = this.props.club.type;
+    const contact = this.props.club.contact;
+    const email = this.props.club.email;
+    const description = this.props.club.description;
+    const _id = this.props.club._id;
+    let { photo } = data;
+    if (photo == null) {
+      photo = 'default';
+    }
+    Favorites.collection.insert(_id, { $set: { name, approve, expire, type, contact, email, description, photo } },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Club added to Favorites', 'success');
+        }
+      });
+  }
+
   render() {
     const clubTypes = [
       { name: 'academic', color: '#129488' },
@@ -60,6 +86,12 @@ class ClubCard extends React.Component {
             </Label>)}
           </Label.Group>
         </Card.Content>
+
+        <Card.Content extra>
+          <Button onClick={data => this.submit(data)}>
+            <Icon name="heart"/>
+          </Button>
+        </Card.Content>
       </Card>
     );
   }
@@ -72,6 +104,9 @@ ClubCard.propTypes = {
     photo: PropTypes.string,
     approve: PropTypes.any,
     expire: PropTypes.any,
+    contact: PropTypes.string,
+    email: PropTypes.string,
+    description: PropTypes.string,
     type: PropTypes.array,
     _id: PropTypes.string,
   }).isRequired,
