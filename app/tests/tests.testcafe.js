b853b030bb5777b682d2931e42ac08816d457a08
jingzhef <jingzhef@hawaii.edu>
2021-12-07 23:33:36 -1000

    update testcafe in issue 15

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -2,23 +2,71 @@ import { landingPage } from './landing.page';
 import { signinPage } from './signin.page';
 import { signoutPage } from './signout.page';
 import { navBar } from './navbar.component';
+import { listClubs } from './listclubs.component';
+import { interests } from './interests.component';
+import { listClubsAdmin } from './listClubsAdmin.page.js';
+import { addClubPage } from './addclub.page';
 
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
 const credentials = { username: 'john@foo.com', password: 'changeme' };
+const credentials2 = { username: 'admin@foo.com', password: 'changeme' };
 
 fixture('meteor-application-template-react localhost test with default db')
-    .page('http://localhost:3000');
+  .page('http://localhost:3000');
 
 test('Test that landing page shows up', async (testController) => {
   await landingPage.isDisplayed(testController);
 });
 
-test('Test that signin and signout work', async (testController) => {
+test('Test that signin and signout and contact page work', async (testController) => {
   await navBar.gotoSigninPage(testController);
   await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.isLoggedIn(testController, credentials.username);
+  await navBar.gotoContactPage(testController);
+  await navBar.logout(testController);
+  await signoutPage.isDisplayed(testController);
+});
+
+test('Test that list clubs page and search bar work.', async (testController) => {
+  await navBar.gotoSigninPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await navBar.isLoggedIn(testController, credentials.username);
+  await navBar.gotoListClubPage(testController);
+  await listClubs.gotoSearchResult(testController);
+  await listClubs.gotoClubInfoPage(testController);
+  await navBar.logout(testController);
+  await signoutPage.isDisplayed(testController);
+});
+
+test('Test that interests page work.', async (testController) => {
+  await navBar.gotoSigninPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await navBar.isLoggedIn(testController, credentials.username);
+  await navBar.gotoInterestsPage(testController);
+  await interests.gotoListClubsFilterPage(testController);
+  await navBar.logout(testController);
+  await signoutPage.isDisplayed(testController);
+});
+
+test('Test that edit club page work.', async (testController) => {
+  await navBar.gotoSigninPage(testController);
+  await signinPage.signin(testController, credentials2.username, credentials2.password);
+  await navBar.isLoggedIn(testController, credentials2.username);
+  await navBar.gotoEditClubsPage(testController);
+  await listClubsAdmin.gotoResult(testController);
+  await listClubsAdmin.gotoEditPage(testController);
+  await navBar.logout(testController);
+  await signoutPage.isDisplayed(testController);
+});
+
+test('Test that add club page work.', async (testController) => {
+  await navBar.gotoSigninPage(testController);
+  await signinPage.signin(testController, credentials2.username, credentials2.password);
+  await navBar.isLoggedIn(testController, credentials2.username);
+  await navBar.gotoAddClubPage(testController);
+  await addClubPage.addClub(testController);
   await navBar.logout(testController);
   await signoutPage.isDisplayed(testController);
 });
