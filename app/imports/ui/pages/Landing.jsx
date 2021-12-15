67756a73efb3f0659447c9388a6454602bf8ab26
malialiu <mliu0141@gmail.com>
2021-12-15 13:31:56 -1000

    update greed button function redirect to clubsList page if user signed in already. else it will redirect to signin page.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Button, Grid, Header, Image } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    function checkSignInState() {
+      if (Meteor.userId()) {
+        return '/clubs/all';
+      }
+      return '/signin';
+    }
     return (
       <div className="home-background" id='landing-page'>
         <Grid container verticalAlign="middle" className="home">
@@ -17,7 +24,7 @@ class Landing extends React.Component {
                 UH Mānoa student life offers exciting opportunities to establish lifelong friendships and memories you’ll never forget.
                 Let`s balance your social and academic life by exploring the clubs and organizations that Mānoa offers today!
               </Header>
-              <Button color="green" size="large" as={NavLink} exact to="/signin">Let`s start it here.</Button>
+              <Button color="green" size="large" as={NavLink} exact to={`${checkSignInState()}`}>Let`s start it here.</Button>
             </Grid.Column>
           </Grid.Row>
         </Grid>
